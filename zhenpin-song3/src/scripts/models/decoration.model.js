c0c179ae00e30a006a39802862450a3abc04f1e6
Bob-Jun-Gao <gao6630592@qq.com>
2018-07-27 14:20:06 +0800

    20170727

--- zhenpin-song3/src/scripts/models/decoration.model.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    find(){
-        return fetch('http://39.106.187.52:8080/ssh1fs/mock/decoration.json')
-        .then(response => response.json())
-        .then(result => {
-            return result
-        })
-    }
-}
